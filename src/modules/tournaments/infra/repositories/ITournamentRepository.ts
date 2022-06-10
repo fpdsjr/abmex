bb2e4d79e8a4b378f403549ebc0cb460ca1263e4
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:29:15 -0300

    feat: add ITournamentoRepository

---
+++ src/modules/tournaments/infra/repositories/ITournamentRepository.ts
@@ -0,0 +1,24 @@
+import { ICreateTournamentDTO } from '../../dtos/ICreateTournamentDTO';
+import { IUpdateTournamentDTO } from '../../dtos/IUpdateTournamentDTO';
+import { Tournament } from '../entities/tournament';
+
+interface ITournamentRepository {
+  createTournament: ({
+    nome,
+    descricao,
+    premiacao,
+  }: ICreateTournamentDTO) => Promise<Tournament>;
+
+  listTournamentById: (id: string) => Promise<Tournament | null>;
+
+  listAllTournament: () => Promise<Tournament[]>;
+
+  updateTournament: ({
+    id,
+    nome,
+    descricao,
+    premiacao,
+  }: IUpdateTournamentDTO) => Promise<Tournament>;
+}
+
+export { ITournamentRepository };

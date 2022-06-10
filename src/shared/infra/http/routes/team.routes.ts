ae1f82dd78047f21cfeda520587874cd378e2862
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:56:13 -0300

    feat: add update team route

--- src/shared/infra/http/routes/team.routes.ts
+++ src/shared/infra/http/routes/team.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateTeamController } from '../../../../modules/teams/useCases/createTeam/CreateTeamController';
+import { UpdateTeamController } from '../../../../modules/teams/useCases/updateTeam/UpdateTeamController';
 
 const teamRoute = Router();
 
 const createTeamController = new CreateTeamController();
+const updateTeamController = new UpdateTeamController();
 
-teamRoute.post('/create/team/:id', createTeamController.handle);
+teamRoute.post('/team/create/:id', createTeamController.handle);
+
+teamRoute.post('/team/update/:id', updateTeamController.handle);
 
 export { teamRoute };

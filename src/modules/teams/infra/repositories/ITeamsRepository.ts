87f5d040056d55db8a6a0a1e3d0f7e0ba91b0bc4
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:55:50 -0300

    feat: add ITeamsRepository

--- src/modules/teams/infra/repositories/ITeamsRepository.ts
+++ src/modules/teams/infra/repositories/ITeamsRepository.ts
@@ -1,8 +1,10 @@
 import { ICreateTeamsDTO } from '../../dtos/ICreateTeamsDTO';
+import { IUpdateTeamsDTO } from '../../dtos/IUpdateTeamDTO';
 import { Team } from '../entities/teams';
 
 interface ITeamsRepository {
   createTeam: ({ id, nome, iniciais }: ICreateTeamsDTO) => Promise<Team>;
+  updateTeam: ({ id, nome, iniciais }: IUpdateTeamsDTO) => Promise<Team>;
 }
 
 export { ITeamsRepository };

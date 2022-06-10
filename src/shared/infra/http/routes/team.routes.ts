837c1eeb67e5d0ba6ba7f2ce0cbae29434aba6e9
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 17:50:11 -0300

    feat: add listAllTeams routes

--- src/shared/infra/http/routes/team.routes.ts
+++ src/shared/infra/http/routes/team.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
 import { CreateTeamController } from '../../../../modules/teams/useCases/createTeam/CreateTeamController';
+import { ListAllTeamsController } from '../../../../modules/teams/useCases/listAllTeams/ListAllTeamsController';
 import { UpdateTeamController } from '../../../../modules/teams/useCases/updateTeam/UpdateTeamController';
 
 const teamRoute = Router();
 
 const createTeamController = new CreateTeamController();
 const updateTeamController = new UpdateTeamController();
+const listAllTeams = new ListAllTeamsController();
 
 teamRoute.post('/team/create/:id', createTeamController.handle);
 
 teamRoute.post('/team/update/:id', updateTeamController.handle);
+
+teamRoute.get('/team/listall/', listAllTeams.handle);
 
 export { teamRoute };

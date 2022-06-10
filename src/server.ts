7c8b8fb685bcf899f6e92122021367452ffc3d3d
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:27:27 -0300

    feat: add teamRoute

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import './shared/container';
 
+import { teamRoute } from './shared/infra/http/routes/team.routes';
 import { tournamentRoutes } from './shared/infra/http/routes/tournament.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(teamRoute);
 
 app.use(tournamentRoutes);
 

117f6256ad1a8a2efcffb2f562279b64c54fcfb1
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:30:46 -0300

    feat: add teamsrepository singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
-import { ITournamentRepository } from '../../modules/tournaments/infra/ITournamentRepository';
+import { TeamsRepository } from '../../modules/teams/infra/repositories/implementations/TeamsRepository';
+import { ITeamsRepository } from '../../modules/teams/infra/repositories/ITeamsRepository';
 import { TournamentRepository } from '../../modules/tournaments/infra/repositories/implementations/TournamentRepository';
+import { ITournamentRepository } from '../../modules/tournaments/infra/repositories/ITournamentRepository';
 
 container.registerSingleton<ITournamentRepository>(
   'TournamentRepository',
   TournamentRepository,
+);
+
+container.registerSingleton<ITeamsRepository>(
+  'TeamsRepository',
+  TeamsRepository,
 );

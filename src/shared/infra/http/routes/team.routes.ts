c1418e07dac6fbc91e09b4f6b99640e73e5c48c6
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 18:16:00 -0300

    feat: add find team by id route

--- src/shared/infra/http/routes/team.routes.ts
+++ src/shared/infra/http/routes/team.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateTeamController } from '../../../../modules/teams/useCases/createTeam/CreateTeamController';
+import { FindTeamByIdController } from '../../../../modules/teams/useCases/findTeamById/FindTeamByIdController';
 import { ListAllTeamsController } from '../../../../modules/teams/useCases/listAllTeams/ListAllTeamsController';
 import { UpdateTeamController } from '../../../../modules/teams/useCases/updateTeam/UpdateTeamController';
 
@@ -9,11 +10,14 @@ const teamRoute = Router();
 const createTeamController = new CreateTeamController();
 const updateTeamController = new UpdateTeamController();
 const listAllTeams = new ListAllTeamsController();
+const findTeamById = new FindTeamByIdController();
 
 teamRoute.post('/team/create/:id', createTeamController.handle);
 
 teamRoute.post('/team/update/:id', updateTeamController.handle);
 
 teamRoute.get('/team/listall/', listAllTeams.handle);
+
+teamRoute.get('/team/:id', findTeamById.handle);
 
 export { teamRoute };

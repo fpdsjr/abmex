c6f9529eb5d2c6f9f17335064b43e3cd60829fb4
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 02:50:07 -0300

    feat: add tournamentRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import 'reflect-metadata';
 import express from 'express';
+import './shared/container';
+
+import { tournamentRoutes } from './shared/infra/http/routes/tournament.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(tournamentRoutes);
 
 app.listen(3999, () => {
   console.log('App Running');

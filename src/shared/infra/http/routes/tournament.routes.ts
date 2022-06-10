c9007df2462a712777be720b57cd786ca4029398
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 16:30:34 -0300

    feat: add updatetournament route

--- src/shared/infra/http/routes/tournament.routes.ts
+++ src/shared/infra/http/routes/tournament.routes.ts
@@ -3,18 +3,25 @@ import { Router } from 'express';
 import { CreateTournamentController } from '../../../../modules/tournaments/useCases/createTournament/CreateTournamentController';
 import { ListAllTournamentController } from '../../../../modules/tournaments/useCases/listAllTournament/ListAllTournamentController';
 import { ListTournamentByIdController } from '../../../../modules/tournaments/useCases/listTournamentById/listTournamentByIdController';
+import { UpdateTournamentController } from '../../../../modules/tournaments/useCases/updateTournament/UpdateTournamentController';
 
 const tournamentRoutes = Router();
 
 const createTournamentController = new CreateTournamentController();
 const listAllTournamentController = new ListAllTournamentController();
 const listTournamentByIdController = new ListTournamentByIdController();
+const updateTournamentController = new UpdateTournamentController();
 
 tournamentRoutes.post('/tournament/create', createTournamentController.handle);
 
 tournamentRoutes.get(
   '/tournament/list/:id',
   listTournamentByIdController.handle,
+);
+
+tournamentRoutes.post(
+  '/tournament/update/:id',
+  updateTournamentController.handle,
 );
 
 tournamentRoutes.get('/tournament/listall', listAllTournamentController.handle);

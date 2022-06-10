7a7161ff09e971478aee2ace8ac54cf293588957
fpdsjr <flaviopds.jr@gmail.com>
2022-06-10 02:51:32 -0300

    feat: add create tournament

---
+++ src/modules/tournaments/useCases/createTournament/CreateTournamentController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateTournamentUseCase } from './CreateTournamentUseCase';
+
+class CreateTournamentController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { nome, descricao, premiacao } = request.body;
+
+    const createTournamentUseCase = container.resolve(CreateTournamentUseCase);
+
+    const createTournament = await createTournamentUseCase.execute({
+      nome,
+      descricao,
+      premiacao,
+    });
+
+    return response.status(200).json(createTournament);
+  }
+}
+
+export { CreateTournamentController };
---
+++ src/modules/tournaments/useCases/createTournament/CreateTournamentUseCase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateTournamentDTO } from '../../dtos/ICreateTournamentDTO';
+import { Tournament } from '../../infra/entities/tournament';
+import { ITournamentRepository } from '../../infra/ITournamentRepository';
+
+@injectable()
+class CreateTournamentUseCase {
+  constructor(
+    @inject('TournamentRepository')
+    private tournamentRepository: ITournamentRepository,
+  ) {}
+
+  async execute({
+    nome,
+    descricao,
+    premiacao,
+  }: ICreateTournamentDTO): Promise<Tournament> {
+    const createTournament = await this.tournamentRepository.createTournament({
+      nome,
+      descricao,
+      premiacao,
+    });
+
+    return createTournament;
+  }
+}
+
+export { CreateTournamentUseCase };

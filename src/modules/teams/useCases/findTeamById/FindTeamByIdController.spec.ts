41fbcde8d71d02c9476a1179341a2d23f237c98a
fpdsjr <flaviopds.jr@gmail.com>
2022-06-13 03:38:06 -0300

    test: add findTeamById

---
+++ src/modules/teams/useCases/findTeamById/FindTeamByIdController.spec.ts
@@ -0,0 +1,26 @@
+import { prisma } from '~/shared/infra/database/prisma';
+import { app } from '~/shared/infra/http/app';
+import request from 'supertest';
+
+describe('Delete Team Controller', () => {
+  afterAll(async () => {
+    await prisma.campeonato.deleteMany();
+  });
+
+  it('should be able to delete a team', async () => {
+    const createTournament = await request(app).post('/tournament/create').send({
+      nome: 'champions',
+      descricao: 'maior campeonato da europa',
+      premiacao: 45221,
+    });
+
+    const createdTeam = await request(app).post(`/team/create/${createTournament.body.id}`).send({
+      nome: 'FLAMENGO',
+      iniciais: 'FLA',
+    });
+
+    const response = await request(app).delete(`/team/delete/${createdTeam.body.id}`);
+
+    expect(response.status).toBe(200);
+  });
+});
